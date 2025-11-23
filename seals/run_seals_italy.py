7e6bd9648307b9414711151923cc251e1bbe2b9d
Chiara Colesanti Senni <chiacole@hotmail.it>
2025-11-23 14:24:21 -0500

    user guide final

--- seals/run_seals_italy.py
+++ seals/run_seals_italy.py
@@ -21,7 +21,13 @@ if __name__ == '__main__':
     # files that already exist.
     p.user_dir = os.path.expanduser('~')
     p.extra_dirs = ['Files', 'seals', 'projects']
-    p.project_name = 'italy_ssp2_rcp45_run'
+    current_csv_name = 'italy_ssp2_rcp45.csv'
+    project_folder_name = current_csv_name.replace('.csv', '') + '_run'
+
+    p.project_name = project_folder_name
+
+    current_script_folder = os.path.dirname(os.path.abspath(__file__))
+    main_repo_folder = os.path.dirname(current_script_folder)
     #p.project_name = p.project_name + '_' + hb.pretty_time() # If don't you want to recreate everything each time, comment out this line.
     # Based on the paths above, set the project_dir. All files will be created in this directory.
     p.project_dir = os.path.join(p.user_dir, os.sep.join(p.extra_dirs), p.project_name)
@@ -50,9 +56,13 @@ if __name__ == '__main__':
     # A useful way to get started is to to run SEALS on the test data without modification
     # and then edit the scenario_definitions.csv to your project needs.   
     #p.scenario_definitions_filename = 'standard_scenarios.csv' 
-    p.scenario_definitions_filename = 'italy_ssp2_rcp45.csv'
+    #p.scenario_definitions_filename = 'italy_ssp2_rcp45.csv'
     #p.scenario_definitions_path = os.path.join(p.input_dir, p.scenario_definitions_filename)
-    p.scenario_definitions_path = os.path.join(p.cur_dir, '..', 'scenarios', p.scenario_definitions_filename)
+    #p.scenario_definitions_path = os.path.join(p.cur_dir, '..', 'scenarios', p.scenario_definitions_filename)
+    p.scenario_definitions_filename = current_csv_name
+    p.scenario_definitions_path = os.path.join(main_repo_folder, 'scenarios', current_csv_name)
+    if not os.path.exists(p.scenario_definitions_path):
+        raise FileNotFoundError(f"CRITICAL: Could not find {p.scenario_definitions_path}")
     seals_initialize_project.initialize_scenario_definitions(p)
         
     # Set processing resolution: determines how large of a chunk should be processed at a time. 4 deg is about max for 64gb memory systems
